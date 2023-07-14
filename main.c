f988d0aa6cc85b0da3018df8d1dd800deacd5b1b
MahmoudEmad1491999 <mahmoud149emad@gmail.com>
2023-07-14 23:59:35 +0300

    removed for the second time .cache files.

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdint.h>
+
+void test(void* x, void* y)
+{
+    printf("%d, %d\n", *((int32_t*)(x)), *((int32_t*)(y)));
+}
+
 
 int main(int argc, char* argv[])
 {
-
+    int32_t x = 200, y = 300; 
+    void (*test_shadow)(int32_t*, int32_t*);
+    test_shadow = (void (*)(int32_t*, int32_t*))test;
+    test_shadow(&x, &y);
     return 0;
 }
 
